60e3d4aef3ac65bfef64e2a29d928a00cb5c3cd4
GPM <javiergenepaul@gmail.com>
2026-04-01 15:10:47 +0800

    feat: refactor naming routes and side bar label from leave to request

--- app/dashboard/employee/leave/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LeaveSection } from "@/components/dashboard/employee/leave"
-
-export default function LeavePage() {
-  return (
-    <div className="p-6 animate-in fade-in duration-300">
-      <LeaveSection />
-    </div>
-  )
-}
---
+++ app/dashboard/employee/request/page.tsx
@@ -0,0 +1,9 @@
+import { RequestSection } from "@/components/dashboard/employee/request"
+
+export default function RequestPage() {
+  return (
+    <div className="p-6 animate-in fade-in duration-300">
+      <RequestSection />
+    </div>
+  )
+}
